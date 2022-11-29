1c55bcd545325bcc5828a08982562804cca84630
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2022-11-29 14:51:12 +0100

    fix

--- Arbre/settings.py
+++ Arbre/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media/applicant_img"
+MEDIA_URL = "/media/img_applicant/"
+MEDIA_ROOT = BASE_DIR / "media/img_applicant/"
 AUTH_USER_MODEL = "account_simplonien.Simplonien"
